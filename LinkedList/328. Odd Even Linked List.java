ff26411bced485e4ff01f030e4863a7a9510d8f3
Fan Luo <fluo4@stevens.edu>
2022-05-10 14:50:33 -0400

    Create 328. Odd Even Linked List.java

---
+++ LinkedList/328. Odd Even Linked List.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        
+        ListNode oddCur = head, cur = head, evenCur = head.next, evenHead = head.next;
+        int i = 1;
+        
+        while (cur != null) {
+            if (i > 2 && i % 2 != 0) {
+                oddCur.next = cur;
+                oddCur = oddCur.next;
+            } else if (i > 2 && i % 2 == 0){
+                evenCur.next = cur;
+                evenCur = evenCur.next;
+            }
+            cur = cur.next;
+            i += 1;
+        }
+        
+        oddCur.next = evenHead;
+        evenCur.next = null;
+        
+        return head;
+        
+    }
+}
