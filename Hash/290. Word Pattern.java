c4bfa3578d3f89098f28309a752a28b1d44d5c92
Fan Luo <fluo4@stevens.edu>
2022-05-10 14:02:13 -0400

    Create 290. Word Pattern.java

---
+++ Hash/290. Word Pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+        HashMap<Character, String> map = new HashMap<>();
+        
+        for (int i = 0; i < pattern.length(); i ++) {
+            if (!map.containsKey(pattern.charAt(i))) {
+                if (map.containsValue(words[i])) {
+                    return false;
+                }
+                map.put(pattern.charAt(i), words[i]);
+            } else {
+                if (!map.get(pattern.charAt(i)).equals(words[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
