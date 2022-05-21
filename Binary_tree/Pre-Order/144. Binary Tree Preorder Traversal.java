af27e38e64639f7a6fe65904b097fc25534b07a0
Fan Luo <fluo4@stevens.edu>
2022-05-21 11:59:47 -0400

    Create 144. Binary Tree Preorder Traversal.java

---
+++ Binary_tree/Pre-Order/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        List<Integer> res = new ArrayList<>();
+        
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return res;
+    }
+}
