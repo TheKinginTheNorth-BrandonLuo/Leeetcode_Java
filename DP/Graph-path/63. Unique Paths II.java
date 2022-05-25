f2eaab4bbc48bc8aafb900d72cdeada95eb0d14f
Fan Luo <fluo4@stevens.edu>
2022-05-25 13:51:47 -0400

    Create 63. Unique Paths II.java

---
+++ DP/Graph-path/63. Unique Paths II.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int ROWS = obstacleGrid.length, COLS = obstacleGrid[0].length;
+        int[][] dp = new int[ROWS][COLS];
+        
+        for (int[] row: dp) {
+            Arrays.fill(row, 0);
+        }
+        
+        for (int c = 0; c < COLS; c ++) {
+            if (obstacleGrid[0][c] == 1) {
+                break;
+            }
+            dp[0][c] = 1;
+        }
+        
+        for (int r = 0; r < ROWS; r ++) {
+            if (obstacleGrid[r][0] == 1) {
+                break;
+            }
+            dp[r][0] = 1;
+        }
+        
+        for (int r = 1; r < ROWS; r ++) {
+            for (int c = 1; c < COLS; c ++) {
+                if (obstacleGrid[r][c] == 1) {
+                    continue;
+                }
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+            }
+        }
+        return dp[ROWS - 1][COLS - 1];
+    }
+}
