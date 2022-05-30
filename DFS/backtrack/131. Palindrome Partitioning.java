140b3c0ec4d2fec541aaa12af38c8ed4cb512d0e
Fan Luo <fluo4@stevens.edu>
2022-05-30 15:41:28 -0400

    Create 131. Palindrome Partitioning.java

---
+++ DFS/backtrack/131. Palindrome Partitioning.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        backtrack(s, res, new ArrayList<>(), 0);
+        return res;
+    }
+    
+    private void backtrack(String s, List<List<String>> res, List<String> path, int start) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(path));
+        }
+        
+        for (int i = start; i < s.length(); i ++) {
+            if (isPalindrome(s, start, i)) {
+                path.add(s.substring(start, i + 1));
+                backtrack(s, res, path, i + 1);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+    
+    private boolean isPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low ++) != s.charAt(high --)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
