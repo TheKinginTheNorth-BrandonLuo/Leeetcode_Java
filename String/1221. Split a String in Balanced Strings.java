6e0ec089ec8bdd3dbfcdf4808750ab721ef65d3b
Fan Luo <fluo4@stevens.edu>
2022-06-06 16:32:57 -0400

    Create 1221. Split a String in Balanced Strings.java

---
+++ String/1221. Split a String in Balanced Strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int L_count = 0;
+        int R_count = 0;
+        int res = 0;
+        //String[] substrings = s.split("");
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                L_count += 1;
+            } else {
+                R_count += 1;
+            }
+            if (L_count == R_count) {
+                res += 1;
+            }
+        }
+        return res;
+    }
+}
