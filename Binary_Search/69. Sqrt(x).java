7721ab8622b36bcb093502e27a452f080eda080f
Fan Luo <fluo4@stevens.edu>
2022-06-12 11:15:17 -0400

    Create 69. Sqrt(x).java

---
+++ Binary_Search/69. Sqrt(x).java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        // note prevent overflow we should use / rather *
+        if (x < 2) {
+            return x;
+        }
+        
+        int left = 1, right = x;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) {
+                return mid;
+            }
+            else if (mid > x / mid) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
